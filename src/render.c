ea9a6abbffd83f6c78e8d552d8cc56e958824906
yorn <yornclaudel@gmail.com>
2021-11-19 18:54:05 +0100

    working on render

--- src/render.c
+++ src/render.c
@@ -23,21 +23,74 @@ static void render_loop(GLFWwindow* window)
 	}
 }
 
+static int obj_to_vertex_buffer(Obj* obj, Context* ctx)
+{
+	size_t nv = ft_lstsize(obj->vertices);
+	float* buff = ft_calloc(1, nv * sizeof(GL_FLOAT) * 3); // times 3 causes 3 float attribute per vertex
+
+	if(buff == NULL)
+		return EXIT_FAILURE;
+	t_list* el = obj->vertices;
+	vertex* v;
+	int i = 0;
+	while (el)
+	{
+		v = (vertex*)el->content;
+		el = el->next;
+		buff[i] = v->x;
+		buff[i + 1] = v->y;
+		buff[i + 2] = v->z;
+		i++;	
+	}
+	ctx->vertex_buffer.content = buff;
+	ctx->vertex_buffer.size = nv * GL_FLOAT * 3;
+	return EXIT_SUCCESS;
+}
+
+static void init_context(Obj* obj, Context *ctx)
+{
+	ctx->obj = obj;
+	ctx->vertex_buffer.content = NULL;
+	ctx->vertex_buffer.size = 0;
+	ctx->index_buffer.content = NULL;
+	ctx->index_buffer.size = 0;
+
+}
+
+static void clear_context(Context* ctx)
+{
+	if (ctx->vertex_buffer.content)
+		free(ctx->vertex_buffer.content);
+	if (ctx->index_buffer.content)
+		free(ctx->index_buffer.content);
+}
+
+// Returns EXIT_FAILURE after printing msg on stderr and clearing context
+static int render_error(const char* msg, Context* ctx)
+{
+        fprintf(stderr, "%s", msg);
+	clear_context(ctx);
+        glfwTerminate();
+	return EXIT_FAILURE;
+}
+
 int render(Obj* obj)
 {
-	(void) obj;
+	// Init context
+	Context ctx;
+	init_context(obj, &ctx);
+
 	GLFWwindow* window = init_window();
-        if (window == NULL) {
-          fprintf(stderr, "Failed to create window");
-          glfwTerminate();
-          return EXIT_FAILURE;
-        }
+        if (window == NULL)
+          return render_error("Failed to init window\n", &ctx);
+
         glfwMakeContextCurrent(window);
-        if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-          glfwTerminate();
-          fprintf(stderr, "Cannot instantiate glad\n");
-          return EXIT_FAILURE;
-        }
+        if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+          return render_error("Cannot instantiate glad\n", &ctx);
+
+	if(obj_to_vertex_buffer(obj, &ctx))
+		return EXIT_FAILURE;
         render_loop(window);
+	clear_context(&ctx);
 	return EXIT_SUCCESS;
 }
--- src/render.h
+++ src/render.h
@@ -6,6 +6,17 @@
 #define WINDOW_INIT_HEIGHT 800
 #define WINDOW_INIT_WIDTH 800
 
+typedef struct buff_st {
+	size_t size;
+	void* content;
+} buff_st;
+
+typedef struct ctx_st {
+	Obj* obj;
+	buff_st vertex_buffer;
+	buff_st index_buffer;
+} Context;
+
 int render(Obj* obj);
 
 #endif
