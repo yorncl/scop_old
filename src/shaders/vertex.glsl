37d035167399c31b036f4c1f4bc691e02bacd7bc
Martin CLAUDEL <mclaudel@e2r10p11.clusters.42paris.fr>
2022-01-21 18:36:08 +0100

    working on the projection matrix

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -4,7 +4,30 @@ layout(location = 0) in vec3 position;
 
 out vec2 TexCoord;
 
+
 void main() {
- gl_Position = vec4(position, 1.0);
+
+ mat4 model = mat4 (
+ 	1.0, 0.0, 0.0, 3.0, 
+ 	0.0, 1.0, 0.0, 0.0, 
+ 	0.0, 0.0, 1.0, 0.0, 
+ 	0.0, 0.0, 0.0, 1.0
+ 	);
+
+ mat4 view = mat4 (
+ 	1.0, 0.0, 0.0, 0.0, 
+ 	0.0, 1.0, 0.0, 0.0, 
+ 	0.0, 0.0, 1.0, -3.0, 
+ 	0.0, 0.0, 0.0, 1.0
+ 	);
+
+ mat4 projection = mat4 (
+ 	1.0, 0.0, 0.0, 0.0, 
+ 	0.0, 1.0, 0.0, 0.0, 
+ 	0.0, 0.0, 1.0, 0.0, 
+ 	0.0, 0.0, 0.0, 1.0
+ 	);
+
+ gl_Position =  projection * view * model * vec4(position, 1);
  TexCoord = position.xy;
 }
